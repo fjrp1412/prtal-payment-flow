f8f0c18375f76e2d286d57345102462259629622
Francisco Ruiz <fruz.rc@gmail.com>
2022-10-07 08:50:24 -0400

    add in Due component the change between form and read-only attributes as well as the styles

--- src/components/Due.vue
+++ src/components/Due.vue
@@ -1,33 +1,85 @@
 <template>
-  <div class="due-container">
+  <div class="due-container" v-if="!editing">
     <div class="due-container__circle"></div>
     <div class="due-container__info">
-      <span class="title">{{ title }}</span>
+      <span class="title">{{ mutableDue.title }}</span>
       <span class="amount">
-        <span>{{ amount }} UF</span> (100%)</span
+        <span>{{ mutableDue.amount }} UF</span> ({{
+          mutableDue.percentage
+        }}
+        %)</span
       >
-      <span class="date">{{ date }}</span>
+      <span class="date">{{ mutableDue.date }}</span>
+    </div>
+  </div>
+
+  <div class="due-container" v-else>
+    <div :class="`due-container__circle ${editing && 'highlight'}`"></div>
+    <div class="due-container__editable-info">
+      <input class="editable-title" v-model="mutableDue.title" />
+      <div class="editable-amount">
+        <input type="text" v-model="mutableDue.amount" />
+        <span class="amount__currency">UF</span>
+      </div>
+      <div class="editable-percentage">
+        <button type="button" @click="updatePercentage(-1)">-</button>
+        <span>{{ mutableDue.percentage }} %</span>
+        <button type="button" @click="updatePercentage(1)">+</button>
+      </div>
+
+      <div class="editable-date">
+        <DatePicker
+          class="datepicker"
+          v-model="mutableDue.date"
+          placeholder
+          format="DD/MM/YYYY"
+          value-type="format"
+        ></DatePicker>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import DatePicker from "vue2-datepicker";
+import "vue2-datepicker/index.css";
+
 export default {
   name: "DueComponent",
+  components: {
+    DatePicker,
+  },
   props: {
-    title: {
-      type: String,
+    due: {
+      type: Object,
     },
-    amount: {
-      type: Number,
-    },
-
-    date: {
-      type: String,
+    editing: {
+      type: Boolean,
     },
   },
   data() {
-    return {};
+    return {
+      mutableDue: { ...this.due },
+    };
+  },
+  methods: {
+    updatePercentage(value) {
+      this.mutableDue.percentage += value;
+      if (this.mutableDue.percentage < 0) {
+        this.mutableDue.percentage = 0;
+      } else if (this.mutableDue.percentage > 100) {
+        this.mutableDue.percentage = 100;
+      }
+    },
+  },
+  watch: {
+    mutableDue: {
+      handler(newValue) {
+        console.log(newValue);
+        this.$emit("updateDue", { id: this.due.id, value: newValue });
+      },
+      deep: true,
+    },
   },
 };
 </script>
@@ -43,6 +95,11 @@ export default {
   height: 50px;
   border-radius: 50%;
   background-color: #e2e8f0;
+}
+
+.due-container .highlight {
+  border: solid 2px rgba(52, 96, 220, 1);
+  background-color: white;
 }
 
 .due-container .due-container__info {
@@ -69,5 +126,60 @@ export default {
 .due-container .due-container__info .amount span {
   font-weight: 600;
   font-size: 14px;
+}
+
+.due-container .due-container__editable-info {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+}
+.due-container .due-container__editable-info div {
+  margin: 5px 0px;
+}
+
+.due-container .due-container__editable-info input {
+  width: 120px;
+  margin: 0px 5px;
+  border-radius: 5px;
+  border: solid 0.5px rgba(148, 163, 184, 1);
+  padding: 3px;
+  text-align: center;
+}
+
+.due-container .due-container__editable-info .editable-title {
+  font-weight: 600;
+  font-size: 20px;
+}
+
+.due-container .due-container__editable-info .editable-amount {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.due-container
+  .due-container__editable-info
+  .editable-amount
+  .amount__currency {
+  position: absolute;
+  left: 72%;
+  font-size: 14px;
+  color: rgba(148, 163, 184, 1);
+}
+
+.due-container .due-container__editable-info .editable-percentage button {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  border: 1px solid rgba(52, 96, 220, 1);
+  margin: 0px 5px;
+  color: rgba(52, 96, 220, 1);
+  background: white;
+  font-size: 20px;
+}
+
+.due-container .due-container__editable-info .editable-date .mx-datepicker {
+  width: 150px;
 }
 </style>
